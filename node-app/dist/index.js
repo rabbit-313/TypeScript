141f89c038d97e0a3635864197a0f4b746127b64
rabbit-313 <migiso1051@i.softbank.jp>
2024-06-05 13:14:37 +0900

    add: hit and blow

--- node-app/dist/index.js
+++ node-app/dist/index.js
@@ -35,6 +35,110 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
         if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
     }
 };
+var HitAndBlow = /** @class */ (function () {
+    function HitAndBlow(mode) {
+        this.answerSource = [
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+        ];
+        this.answer = [];
+        this.tryCount = 0;
+        this.neverValue = this.mode;
+        this.mode = mode;
+    }
+    HitAndBlow.prototype.setting = function () {
+        var answerLength = this.getAnswerLength();
+        while (this.answer.length < answerLength) {
+            var randNum = Math.floor(Math.random() * this.answerSource.length);
+            var selectedItem = this.answerSource[randNum];
+            if (!this.answer.includes(selectedItem)) {
+                this.answer.push(selectedItem);
+            }
+        }
+    };
+    HitAndBlow.prototype.end = function () {
+        printLine("\u6B63\u89E3\u3067\u3059\uFF01\n" + this.tryCount + "\u56DE\u76EE\u3067\u30AF\u30EA\u30A2\u3057\u307E\u3057\u305F\uFF01", false);
+        process.exit();
+    };
+    HitAndBlow.prototype.play = function () {
+        return __awaiter(this, void 0, void 0, function () {
+            var answerLength, inputArr, result;
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0:
+                        answerLength = this.getAnswerLength();
+                        return [4 /*yield*/, promptInput("1~9\u306E\u6570\u5B57\u3092" + answerLength + "\u6841\u300C,\u300D\u533A\u5207\u308A\u3067\u5165\u529B\u3057\u3066\u304F\u3060\u3055\u3044\u3002")];
+                    case 1:
+                        inputArr = (_a.sent()).split(",");
+                        if (!!this.validate(inputArr)) return [3 /*break*/, 3];
+                        printLine("入力値が不正です。もう一度入力してください。");
+                        return [4 /*yield*/, this.play()];
+                    case 2:
+                        _a.sent();
+                        return [2 /*return*/];
+                    case 3:
+                        result = this.check(inputArr);
+                        if (!(result.hit !== this.answer.length)) return [3 /*break*/, 5];
+                        printLine("hit: " + result.hit + ", blow: " + result.blow);
+                        this.tryCount += 1;
+                        return [4 /*yield*/, this.play()];
+                    case 4:
+                        _a.sent();
+                        return [3 /*break*/, 6];
+                    case 5:
+                        this.tryCount += 1;
+                        _a.label = 6;
+                    case 6: return [2 /*return*/];
+                }
+            });
+        });
+    };
+    HitAndBlow.prototype.check = function (input) {
+        var _this = this;
+        var hitCount = 0;
+        var blowCount = 0;
+        input.forEach(function (val, index) {
+            if (val === _this.answer[index]) {
+                hitCount += 1;
+            }
+            else if (_this.answer.includes(val)) {
+                blowCount += 1;
+            }
+        });
+        return {
+            hit: hitCount,
+            blow: blowCount
+        };
+    };
+    HitAndBlow.prototype.validate = function (inputArr) {
+        var _this = this;
+        var isLegendValid = inputArr.length === this.answer.length;
+        var isAllAnswerSourceOption = inputArr.every(function (val) {
+            return _this.answerSource.includes(val);
+        });
+        var isAllUnique = inputArr.every(function (val, i) { return inputArr.indexOf(val) === i; });
+        return isLegendValid && isAllAnswerSourceOption && isAllUnique;
+    };
+    HitAndBlow.prototype.getAnswerLength = function () {
+        switch (this.mode) {
+            case "normal":
+                return 3;
+            case "hard":
+                return 4;
+            case "very hard":
+                return 5;
+        }
+    };
+    return HitAndBlow;
+}());
+throw new Error("Unexpected mode: " + neverValue);
 var printLine = function (text, brakeLine) {
     if (brakeLine === void 0) { brakeLine = true; }
     process.stdout.write(text + (brakeLine ? "\n" : ""));
@@ -55,18 +159,16 @@ var promptInput = function (text) { return __awaiter(void 0, void 0, void 0, fun
     });
 }); };
 (function () { return __awaiter(void 0, void 0, void 0, function () {
-    var name, age;
+    var hitAndBlow;
     return __generator(this, function (_a) {
         switch (_a.label) {
-            case 0: return [4 /*yield*/, promptInput("What is your name?")];
+            case 0:
+                hitAndBlow = new HitAndBlow("hard");
+                hitAndBlow.setting();
+                return [4 /*yield*/, hitAndBlow.play()];
             case 1:
-                name = _a.sent();
-                console.log(name);
-                return [4 /*yield*/, promptInput("How old are you?")];
-            case 2:
-                age = _a.sent();
-                console.log(age);
-                process.exit();
+                _a.sent();
+                hitAndBlow.end();
                 return [2 /*return*/];
         }
     });
--- node-app/src/index.ts
+++ node-app/src/index.ts
@@ -1,3 +1,105 @@
+class HitAndBlow {
+  private readonly answerSource: string[] = [
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+  ];
+  private answer: string[] = [];
+  private tryCount = 0;
+  private mode: "normal" | "hard" | "very hard";
+
+  setting() {
+    const answerLength = this.getAnswerLength();
+    while (this.answer.length < answerLength) {
+      const randNum = Math.floor(Math.random() * this.answerSource.length);
+      const selectedItem = this.answerSource[randNum];
+      if (!this.answer.includes(selectedItem)) {
+        this.answer.push(selectedItem);
+      }
+    }
+  }
+
+  constructor(mode: "normal" | "hard") {
+    this.mode = mode;
+  }
+
+  end() {
+    printLine(`正解です！\n${this.tryCount}回目でクリアしました！`, false);
+    process.exit();
+  }
+
+  async play() {
+    const answerLength = this.getAnswerLength();
+    const inputArr = (
+      await promptInput(
+        `1~9の数字を${answerLength}桁「,」区切りで入力してください。`
+      )
+    ).split(",");
+
+    if (!this.validate(inputArr)) {
+      printLine("入力値が不正です。もう一度入力してください。");
+      await this.play();
+      return;
+    }
+
+    const result = this.check(inputArr);
+
+    if (result.hit !== this.answer.length) {
+      printLine(`hit: ${result.hit}, blow: ${result.blow}`);
+      this.tryCount += 1;
+      await this.play();
+    } else {
+      this.tryCount += 1;
+    }
+  }
+
+  private check(input: string[]) {
+    let hitCount = 0;
+    let blowCount = 0;
+
+    input.forEach((val, index) => {
+      if (val === this.answer[index]) {
+        hitCount += 1;
+      } else if (this.answer.includes(val)) {
+        blowCount += 1;
+      }
+    });
+
+    return {
+      hit: hitCount,
+      blow: blowCount,
+    };
+  }
+
+  private validate(inputArr: string[]) {
+    const isLegendValid = inputArr.length === this.answer.length;
+    const isAllAnswerSourceOption = inputArr.every((val) =>
+      this.answerSource.includes(val)
+    );
+    const isAllUnique = inputArr.every((val, i) => inputArr.indexOf(val) === i);
+    return isLegendValid && isAllAnswerSourceOption && isAllUnique;
+  }
+
+  private getAnswerLength() {
+    switch (this.mode) {
+      case "normal":
+        return 3;
+      case "hard":
+        return 4;
+      case "very hard":
+        return 5;
+      default:
+        const neverValue: never = this.mode;
+        throw new Error(`Unexpected mode: ${neverValue}`);
+  }
+}
+
 const printLine = (text: string, brakeLine: boolean = true) => {
   process.stdout.write(text + (brakeLine ? "\n" : ""));
 };
@@ -11,9 +113,8 @@ const promptInput = async (text: string) => {
 };
 
 (async () => {
-  const name = await promptInput("What is your name?");
-  console.log(name);
-  const age = await promptInput("How old are you?");
-  console.log(age);
-  process.exit();
+  const hitAndBlow = new HitAndBlow("hard");
+  hitAndBlow.setting();
+  await hitAndBlow.play();
+  hitAndBlow.end();
 })();
