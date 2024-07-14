dc71a3d918c228a10023fd776e012b886d43b2f1
rabbit-313 <migiso1051@i.softbank.jp>
2024-07-14 17:19:04 +0900

    add: todo task

--- browser-app/ts/index.ts
+++ browser-app/ts/index.ts
@@ -1,8 +1,14 @@
 import { EventListner } from "./EventListner";
 import { Task } from "./Task";
+import { TaskCollection } from "./TaskCollection";
+import { TaskRenderer } from "./TaskRenderer";
 
 class Application {
   private readonly eventListener = new EventListner();
+  private readonly taskCollection = new TaskCollection();
+  private readonly taskRenderer = new TaskRenderer(
+    document.getElementById("todoList") as HTMLElement
+  );
 
   start() {
     const createForm = document.getElementById("createForm") as HTMLFormElement;
@@ -22,6 +28,19 @@ class Application {
     if (!titleInput.value) return;
 
     const task = new Task({ title: titleInput.value });
+    this.taskCollection.add(task);
+    const { deleteButtonEl } = this.taskRenderer.append(task);
+
+    this.eventListener.add(task.id, "click", deleteButtonEl, () =>
+      this.handleClickDeleteTask(task)
+    );
+
+    titleInput.value = "";
+  };
+
+  private handleClickDeleteTask = (task: Task) => {
+    if (!window.confirm(`「${task.title}」を削除しますか？`)) return;
+
     console.log(task);
   };
 }
